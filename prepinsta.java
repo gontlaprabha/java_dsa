18a25903d365ee5f80a685fa732289c4be349585
localrepo <gontlaprabhas02552@gmail.com>
2025-09-25 19:21:56 +0530

    commited

--- prepinsta.java
+++ prepinsta.java
@@ -20,19 +20,61 @@
         
 //     }
 // }
-public class prepinsta{
-    public static int fibo(int a){
-        if(a==0||a==1){
-           return a;
-        }
-        return fibo(a-1)+fibo(a-2);
-    }
+
+// public class prepinsta{
+//     public static int fibo(int a){
+//         if(a==0||a==1){
+//            return a;
+//         }
+//         return fibo(a-1)+fibo(a-2);
+//     }
   
-    public static void main(String[] args) {
-        
-        
+//     public static void main(String[] args) {
+
+//         int seq=10;
+
+//         for (int idx = 0; idx < seq ; idx++) {
+//             System.out.print(fibo(idx)+" ");
+            
+//         }
 
         
         
+//     }
+// }
+
+
+// public class prepinsta{
+//     public static void revnum(int num){
+//         int revnum=0;
+
+//         while(num > 0 ){
+//          int last=num%10;
+//          revnum=revnum*10+last;
+//          num=num/10;
+      
+//         }
+//         System.out.println(revnum);
+        
+//     }
+//     public static void main(String[] args) {
+//         int num=1234;
+
+//         revnum(num);
+
+        
+        
+//     }
+// }
+
+
+public class prepinsta{
+    public static void main(String[]args){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.err.print(j);
+            }
+            System.out.println();
+        }
     }
 }
